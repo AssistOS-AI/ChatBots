18c32d127355dcd3a7dfac7553012196e8eace35
Koroem <conduratvictor@yahoo.com>
2023-12-18 11:11:18 +0200

    added web-components

---
+++ web-components/chatbots-select-personality-page/chatbots-select-personality-page.js
@@ -0,0 +1,23 @@
+export class chatbotsSelectPersonalityPage {
+    constructor(element,invalidate) {
+        this.element = element;
+        this.invalidate=invalidate;
+        this.invalidate();
+    }
+    beforeRender() {
+        this.personalityBlocks = "";
+        if (webSkel.currentUser.space.personalities.length > 0) {
+            webSkel.currentUser.space.personalities.forEach((item) => {
+                this.personalityBlocks += `<personality-unit data-name="${item.name}" data-description="${item.description}" data-id="${item.id}" data-image="${item.image}"></personality-unit>`;
+            });
+        }else {
+            this.personalityBlocks = `<div class="no-personality">No personality defined</div>`;
+        }
+    }
+
+    async selectPersonality(_target){
+        let personality = webSkel.UtilsService.reverseQuerySelector(_target,"personality-unit");
+        let personalityId = personality.getAttribute("data-id");
+        await webSkel.changeToDynamicPage("chatbots-page", `chatbots-page/${personalityId}`);
+    }
+}
