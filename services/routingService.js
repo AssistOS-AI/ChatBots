15ccb7d00934e5de598abd9e540189718c559be2
Koroem <conduratvictor@yahoo.com>
2024-01-11 09:26:18 +0200

    small fix

--- services/routingService.js
+++ services/routingService.js
@@ -1,17 +1,17 @@
 export class routingService {
     constructor() {}
     async navigateToLocation(locationArray = [], appName) {
-        const CHATBOTS_PAGE = "chatbots-select-personality-page";
+        const CHATBOTS_SELECT_PERSONALITY_PAGE = "chatbots-select-personality-page";
         const PERSONALITY_PAGE = "personality";
-
+	const CHATBOTS_PAGE ="chatbots-page"
         
-        if (locationArray.length === 0 || locationArray[0] === CHATBOTS_PAGE) {
-            const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${CHATBOTS_PAGE}`;
-            await webSkel.changeToDynamicPage(CHATBOTS_PAGE, pageUrl);
+        if (locationArray.length === 0 || locationArray[0] === CHATBOTS_SELECT_PERSONALITY_PAGE ) {
+            const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${CHATBOTS_SELECT_PERSONALITY_PAGE }`;
+            await webSkel.changeToDynamicPage(CHATBOTS_SELECT_PERSONALITY_PAGE, pageUrl);
             return;
         }
-        if(locationArray[locationArray.length-1]!== PERSONALITY_PAGE){
-         console.error(`Invalid URL: URL must end with ${CHATBOTS_PAGE} or ${PERSONALITY_PAGE}`);
+        if(locationArray[locationArray.length-1]!== CHATBOTS_PAGE){
+         console.error(`Invalid URL: URL must end with ${CHATBOTS_PAGE} or ${CHATBOTS_SELECT_PERSONALITY_PAGE}`);
             return;
         }
 
